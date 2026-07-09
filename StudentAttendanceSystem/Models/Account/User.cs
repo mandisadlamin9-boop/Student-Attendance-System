48598dad0abede9612a64eb3c1a1fe8dfe909fc1
Lihle <mandisadlamin9@gmail.com>
2026-07-09 06:34:15 -1200

    Create User model and UserRole enum

---
+++ StudentAttendanceSystem/Models/Account/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using StudentAttendanceSystem.Utilities.Enums;
+
+namespace StudentAttendanceSystem.Models.Account
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [StringLength(100)]
+        public string Email { get; set; }
+
+        [Required]
+        public string PasswordHash { get;set; }
+
+        [Required]
+        [StringLength(20)]
+        public UserRole Role { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+
+}
---
+++ StudentAttendanceSystem/Utilities/Enums/UserRole.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentAttendanceSystem.Utilities.Enums
+{
+    public enum UserRole
+    {
+        Admin = 1,
+        Lecturer = 2
+    }
+}
