5902cf50f83a4a89564e354dc04715dece31cec4
Lihle <mandisadlamin9@gmail.com>
2026-07-09 07:09:53 -1200

    Add Attendance model and AttendanceStatus enum

---
+++ StudentAttendanceSystem/Models/Attendance/Attendance.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using StudentAttendanceSystem.Utilities.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace StudentAttendanceSystem.Models.Attendance
+{
+    public class Attendance
+    {
+        [Key]
+        public int AttendanceId { get; set; }
+
+        [Required]
+        public int StudentId { get; set; }
+
+        [Required]
+        public int CourseId { get; set; }
+
+        [Required]
+        public DateTime AttendanceDate { get; set; }
+
+        [Required]
+        public AttendanceStatus Status { get; set; }
+
+        [StringLength(250)]
+        public string Remarks { get; set; }
+
+        // Navigation Properties
+        public virtual Student.Student Student { get; set; }
+        public virtual Course.Course Course { get; set; }
+    }
+}
---
+++ StudentAttendanceSystem/Utilities/Enums/AttendanceStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentAttendanceSystem.Utilities.Enums
+{
+    public enum AttendanceStatus
+    {
+        Present = 1,
+        Absent = 2,
+        Late = 3
+    }
+}
