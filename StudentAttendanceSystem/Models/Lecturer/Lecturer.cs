c1acff842a2b0d7b1a57831496f9e0f116447d84
Lihle <mandisadlamin9@gmail.com>
2026-07-09 06:49:21 -1200

    Add Lecturer model

---
+++ StudentAttendanceSystem/Models/Lecturer/Lecturer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace StudentAttendanceSystem.Models.Lecturer
+{
+    public class Lecturer
+    {
+        [Key]
+        public int LecturerId { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string EmployeeNumber { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [StringLength(100)]
+        public string Email { get; set; }
+
+        [Phone]
+        [StringLength(15)]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Department { get; set; }
+    }
+}
