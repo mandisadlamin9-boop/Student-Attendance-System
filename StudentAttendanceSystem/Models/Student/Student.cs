b450cdf6bd9d7a1c229140d16cad1308676fdc97
Lihle <mandisadlamin9@gmail.com>
2026-07-09 06:59:06 -1200

    Add Student model

--- StudentAttendanceSystem/Models/Student/Student.cs
+++ StudentAttendanceSystem/Models/Student/Student.cs
@@ -4,16 +4,17 @@ using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations;
 
-namespace StudentAttendanceSystem.Models.Lecturer
+namespace StudentAttendanceSystem.Models.Student
 {
-    public class Lecturer
+    public class Student
     {
         [Key]
-        public int LecturerId { get; set; }
+        public int StudentId { get; set; }
 
         [Required]
         [StringLength(20)]
-        public string EmployeeNumber { get; set; }
+        public string StudentNumber { get; set; }
+
         [Required]
         [StringLength(50)]
         public string FirstName { get; set; }
@@ -22,16 +23,16 @@ namespace StudentAttendanceSystem.Models.Lecturer
         [StringLength(50)]
         public string LastName { get; set; }
 
-        public int UserId { get; set; }
-
-        public virtual StudentAttendanceSystem.Models.Account.User User { get; set; }
+        [Required]
+        [EmailAddress]
+        [StringLength(100)]
+        public string Email { get; set; }
 
         [Phone]
         [StringLength(15)]
         public string PhoneNumber { get; set; }
 
         [Required]
-        [StringLength(100)]
-        public string Department { get; set; }
+        public int YearLevel { get; set; }
     }
 }
